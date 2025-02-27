4ec6ad069c8cebea931603207100e9f44821cf73
Erdem <erdemsincer123@icloud.com>
2025-02-27 12:28:37 +0300

    EntityTypeConfiguration, ModelCreating, SaveChangesAsync

--- CleanArchitecture.Persistance/Context/AppDbContext.cs
+++ CleanArchitecture.Persistance/Context/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CleanArchitecture.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchitecture.Persistance.Context;
 
@@ -6,5 +7,28 @@ public sealed class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions options) : base(options){}
 
-    
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyReferance).Assembly);
+
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entires = ChangeTracker.Entries<Entity>();
+        foreach (var entry in entires)
+        {
+            if(entry.State == EntityState.Added)
+            {
+                entry.Property(p=>p.CreatedDate)
+                    .CurrentValue=DateTime.Now;
+            }
+
+            if (entry.State == EntityState.Modified)
+            {
+                entry.Property(p => p.UpdatedDate)
+                    .CurrentValue = DateTime.Now;
+            }
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
 }

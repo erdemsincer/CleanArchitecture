a8b4b5d6157b205d0f1889ec2f5764e99ae108e4
Erdem <erdemsincer123@icloud.com>
2025-02-27 12:48:06 +0300

    CQRS Pattern

---
+++ CleanArchitecture.Application/Features/CarFeatures/Commands/CreateCar/CreateCarCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Domain.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Features.CarFeatures.Commands.CreateCar
+{
+    public sealed class CreateCarCommandHandler : IRequestHandler<CreateCarCommand, MessageResponse>
+    {
+        public async Task<MessageResponse> Handle(CreateCarCommand request, CancellationToken cancellationToken)
+        {
+            return new("Araç başarıyla üretildi");
+        }
+    }
+}
--- CleanArchitecture.WebApi/Program.cs
+++ CleanArchitecture.WebApi/Program.cs
@@ -9,6 +9,10 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(conn
 
 builder.Services.AddControllers()
     .AddApplicationPart(typeof(CleanArchitecture.Presentation.AssemblyReference).Assembly);
+
+builder.Services.AddMediatR(cfr =>
+cfr.RegisterServicesFromAssembly(typeof(CleanArchitecture.Application.AssemblyReference).Assembly));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
